8b2e253763f5b0f396d54f83926c7c1d14c9c89f
Shrijanand Chintapatla <shrij@Alienware-13.redmond.corp.microsoft.com>
2018-10-17 21:11:13 -0400

    Including necessary Libraries

---
+++ Server.c
@@ -0,0 +1,17 @@
+// Including necessary libraries
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
