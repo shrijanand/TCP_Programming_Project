fb84f47d7d4d65d9a233190a7cdbb9b19f742b21
Shrijanand Chintapatla <shrij@Alienware-13.redmond.corp.microsoft.com>
2018-10-17 21:21:08 -0400

    Including necessary libraries for Client.c and Main function for Client.c and Server.c

---
+++ Client.c
@@ -0,0 +1,20 @@
+// Including necessary libraries
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Main function
+int main(int argc, char const *argv[]) 
+{
+	return 0;
+}
--- Server.c
+++ Server.c
@@ -11,7 +11,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
-int main(int argc, char *argv[]) {
+// Main function
+int main(int argc, char *argv[]) 
+{
 	return 0;
 }
