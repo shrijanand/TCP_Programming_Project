721a35e3b869594afa10a7287b19587133d08efd
Shrijanand Chintapatla <shrij@Alienware-13.redmond.corp.microsoft.com>
2018-10-18 00:34:18 -0400

    Message_Struct and Get_Message of Message_Struct Data Type

--- Server.c
+++ Server.c
@@ -11,16 +11,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-typedef struct Message {
+extern int errno;
+
+typedef struct Message_Struct 
+{
     uint8_t Transmission_Type;
     uint16_t Length_of_Name;
     uint64_t Size_of_File;
     unsigned char* File;
     unsigned char* Name_of_File;
-}Message;
+}Message_Struct;
+
+Message_Struct Get_Message(int File_Descriptor)
+{
+    Message_Struct Message;
+    Message.Transmission_Type = Get_Transmission_Type(File_Descriptor);
+    Message.Size_of_File = Get_File_Size(File_Descriptor);
+    Message.File = Get_File(File_Descriptor, Message.Size_of_File);
+    Message.Length_of_Name = Get_Length_of_Name(File_Descriptor);
+    Message.Name_of_File  = Get_Name_of_File(File_Descriptor, Message.Length_of_Name);
+    return Message;
+}
 
 // Main function
-int main(int argc, char *argv[]) 
+int main(int Argument_Count, char *Argument_Values[]) 
 {
 	return 0;
 }
